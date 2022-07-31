dcf7825b08180fc4d5659d9de5ba9f1c40c6d950
Mikhail Dro <droydbangs@gmail.com>
2022-08-01 01:28:53 +0300

    feat: complete tack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = -n;
+    }
+    n = n.toString();
+    let reverseN = '';
+    for(let i = n.length - 1; i >=0; i--) {
+      reverseN += n[i];
+    }
+    return(reverseN);
 }
+
